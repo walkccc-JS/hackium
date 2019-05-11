790b8443ec49c52cbe295d457b563c26290adfa7
walkccc <walkccray@gmail.com>
2019-05-11 22:51:11 +0800

    Add client/

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import 'bulma/css/bulma.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
